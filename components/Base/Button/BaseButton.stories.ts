5fcaf12d6e00b60050c8d1f948f11b765e1853f2
HayayoKojima <kojima@digita.co.jp>
2023-12-07 13:39:30 +0900

    init tailwind & styled btn

--- components/Base/Button/BaseButton.stories.ts
+++ components/Base/Button/BaseButton.stories.ts
@@ -14,6 +14,13 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Overview: Story = {
-    args: {},
+export const Default: Story = {
+    render: () => ({
+        components: { BaseButton },
+        template: `
+            <BaseButton>
+                ボタン
+            </BaseButton>
+        `,
+    }),
 }
--- components/Base/Button/index.vue
+++ components/Base/Button/index.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup></script>
 <template>
-    <button>
+    <button class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-400">
         <slot />
     </button>
 </template>
